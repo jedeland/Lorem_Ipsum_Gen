01694a29dddf88262335d2ca4f1d9aefca6db71b
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-09-07 15:36:29 +0200

    Created character class and setout the objectives

---
+++ Character_Gen/addon_character.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+class Character:
+    """
+    This class should be used to create characters in the main running portion, the character should have a name, title and background type as a base type
+    There should be a function to create a timeline and branchoff characters that can be used to create a family inside of a town or region alternatively
+    Characters should be created within a region, and assigned to places to live, then a history of them is created and they interact with one another
+    """
+    def __init__(self):
+        print("Creating new character")
