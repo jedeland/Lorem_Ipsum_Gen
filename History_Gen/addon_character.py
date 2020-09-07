7f0d1f1de539638373ca90d4484badbe7b1592a2
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-09-07 16:13:18 +0200

    Renamed directory

--- History_Gen/addon_character.py
+++ History_Gen/addon_character.py
@@ -7,5 +7,12 @@ class Character:
     There should be a function to create a timeline and branchoff characters that can be used to create a family inside of a town or region alternatively
     Characters should be created within a region, and assigned to places to live, then a history of them is created and they interact with one another
     """
-    def __init__(self):
+    def __init__(self, name, family, background, aspirations, quests):
+        checked_family = self.check_family(self, family)
         print("Creating new character")
+
+    def check_family(self, family):
+        #METHOD: checks the families in a region, if the last name exists then it either adds the characer to the family or creates a new cadet branch, else it creates a new family
+        town_groups = []
+
+
