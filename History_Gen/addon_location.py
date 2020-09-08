38a3f55e554d0ecfa8a19f44c7f4ac26a3966d3c
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-09-08 14:46:07 +0200

    Added functions to set region, get neighbouring regions using a binary tree format and to initialise the tree if it doesnt already exist

--- History_Gen/addon_location.py
+++ History_Gen/addon_location.py
@@ -11,3 +11,24 @@ class Location:
     """
     def __init__(self, town, regional_check, family_name, rank, moto, relations, heraldry):
         print("This function should initialise a family inside of a region")
+
+    def set_region(self, regional_check, culture):
+        #Region types are ranked numerically, to ensure seperate regions can be randomly assigned names
+        #Scope of the regions will go from 1 - 7, starting with kingdom and ending with hamlet / outpost
+        region_types = {1: ["Kingdom", "State", "Lordship", "Grand Duchy", "Viceroyalty"], 2: ["Duchy", "Earldom", "Knezevina", "Duché", "Herzogtum", "Grand Cities", "Principality"],
+                        3: ["County", "Earldom", "League Territory", "Grafschaft", "Comitat"], 4: ["City", "Grand Fortress", "Grand Library", "Capital", "Imperial Free City"],
+                        5: ["Town", "Castle", "University Town", "Market Town", "Grand Harbour", "Renowned Suburb"], 6: ["Village", "Fief", "Abbey", "Fortification", "Barony", "Barracks"],
+                        7: ["Hamlet", "Settlement", "Hunters Lodge", "Outpost", "Chapel"]}
+        target_culture = culture
+    def get_neighbouring_regions(self, region_nodes, neighbours):
+        if region_nodes:
+            print("Regions have been created, connecting new location to others")
+        else:
+            initialise_regions()
+
+
+def initialise_regions():
+    #import namegen functions and name binary trees after generated regions, create single use name generator based on european names and add the name and respective culture to binary tree to be used in setting regions
+    region_number = 
+    binary_trees = {}
+    for i in range(10):
